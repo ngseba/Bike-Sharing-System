586848ba87f246d8c9955b5169dbd6bfc5b9462c
george neamt <georgeneamt2004@yahoo.com>
2020-01-10 20:18:29 +0200

    add intellij project with structure described at course

---
+++ src/main/java/ro/iteahome/bikeshare/Application.java
@@ -0,0 +1,7 @@
+package ro.iteahome.bikeshare;
+
+public class Application {
+
+    public static void main(String[] args){
+    }
+}
---
+++ src/main/java/ro/iteahome/bikeshare/model/Bike.java
@@ -0,0 +1,18 @@
+package ro.iteahome.bikeshare.model;
+
+public class Bike {
+
+    private int id;
+
+    public Bike(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
